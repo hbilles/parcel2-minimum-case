0bbb469a39294b44734989a60299e60b8a157d47
Hite Billes <hite@billes.io>
2021-06-16 17:13:49 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './styles.styl'
+import picard from 'url:./assets/facepalm.jpg'
+
+const Foo = () => (
+  <img src={picard} alt='logo' />
+)
+
+export default Foo
